3e0129b87df9e43c8bc3f4a1563ff83339cd9844
Shreshta <shrestashiny05@gmail.com>
2025-03-21 04:31:13 -0400

    fixed App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,32 +9,24 @@ import StudentAuth from './pages/StudentAuth';
 import ProfessorCourses from './pages/ProfessorCourses';
 import StudentDashboard from './pages/StudentDashboard';
 
-// const App = () => {
-//   return (
-//     <ThemeProvider theme={theme}>
-//       <CssBaseline /> {/* Applies global Material UI reset styles */}
-//       <Routes>
-//         <Route path="/" element={<LandingPage />} />
-
-//         {/* Professor routes */}
-//         <Route path="/professor-login" element={<ProfessorAuth />} />
-//         <Route path="/professor-dashboard" element={<ProfessorDashboard />} />
-
-//         {/* Student routes */}
-//         <Route path="/student-login" element={<StudentAuth />} />
-//         <Route path="/student-dashboard" element={<StudentDashboard />} />
-//       </Routes>
-//     </ThemeProvider>
-//   );
-// };
-
 const App = () => {
   return (
-    <Routes>
-      {/* Temporarily set ProfessorCourses as the default page */}
-      <Route path="/" element={<ProfessorCourses />} />
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <CssBaseline /> {/* Applies global Material UI reset styles */}
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+
+        {/* Professor routes */}
+        <Route path="/professor-login" element={<ProfessorAuth />} />
+        <Route path="/professor-dashboard" element={<ProfessorDashboard />} />
+
+        {/* Student routes */}
+        <Route path="/student-login" element={<StudentAuth />} />
+        <Route path="/student-dashboard" element={<StudentDashboard />} />
+      </Routes>
+    </ThemeProvider>
   );
 };
+
 
 export default App;
