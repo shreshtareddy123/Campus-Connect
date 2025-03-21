53ac3c1f00e98efcfeab52155f74694f5b3e1a12
Shreshta <sreshtashiny05@gmail.com>
2025-03-21 05:34:47 -0400

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import { ThemeProvider, CssBaseline } from '@mui/material';
-import theme from './theme';
+import { CssBaseline } from '@mui/material';
 
 import LandingPage from './pages/LandingPage';
 import ProfessorAuth from './pages/ProfessorAuth';
@@ -10,30 +9,24 @@ import StudentDashboard from './pages/StudentDashboard';
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline /> {/* Applies global Material UI reset styles */}
+    <>
+      <CssBaseline /> {/* Normalizes Material UI styles */}
       <Routes>
+        {/* Landing Page */}
         <Route path="/" element={<LandingPage />} />
 
         {/* Professor Routes */}
         <Route path="/professor-login" element={<ProfessorAuth />} />
-        <Route path="/professor-signup" element={<ProfessorAuth />} /> 
+        <Route path="/professor-signup" element={<ProfessorAuth />} />
         <Route path="/professor-dashboard" element={<ProfessorDashboard />} />
 
-        {/* Fix: Make sure these Student routes exist */}
+        {/* Student Routes */}
         <Route path="/student-login" element={<StudentAuth />} />
         <Route path="/student-signup" element={<StudentAuth />} />
-        {/* Professor routes */}
-        <Route path="/professor-login" element={<ProfessorAuth />} />
-        <Route path="/professor-dashboard" element={<ProfessorDashboard />} />
-
-        {/* Student routes */}
-        <Route path="/student-login" element={<StudentAuth />} />
         <Route path="/student-dashboard" element={<StudentDashboard />} />
       </Routes>
-    </ThemeProvider>
+    </>
   );
 };
-
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
 import App from './App';
-import theme from './theme'; // ← Your custom theme
-
 import './index.css'; // Optional global styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <CssBaseline /> {/* Normalizes CSS */}
-        <App />
-      </ThemeProvider>
+      <CssBaseline /> {/* Normalizes CSS */}
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
