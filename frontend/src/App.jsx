fd2360c4f8a8fa8e65abe1a7871d1c4274169f3d
Shreshta <sreshtashiny05@gmail.com>
2025-03-21 20:26:05 -0400

    fixes on landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,28 @@
 import { Routes, Route } from 'react-router-dom';
-import { CssBaseline } from '@mui/material';
-
 import LandingPage from './pages/LandingPage';
 import ProfessorAuth from './pages/ProfessorAuth';
 import ProfessorDashboard from './pages/ProfessorDashboard';
 import StudentAuth from './pages/StudentAuth';
 import StudentDashboard from './pages/StudentDashboard';
+import ProfessorCourses from './pages/ProfessorCourses';
 
 const App = () => {
   return (
-    <>
-      <CssBaseline /> {/* Normalizes Material UI styles */}
-      <Routes>
-        {/* Landing Page */}
-        <Route path="/" element={<LandingPage />} />
+    <Routes>
+      {/* Landing Page */}
+      <Route path="/" element={<LandingPage />} />
 
-        {/* Professor Routes */}
-        <Route path="/professor-login" element={<ProfessorAuth />} />
-        <Route path="/professor-signup" element={<ProfessorAuth />} />
-        <Route path="/professor-dashboard" element={<ProfessorDashboard />} />
+      {/* Professor Routes */}
+      <Route path="/professor-login" element={<ProfessorAuth />} />
+      <Route path="/professor-signup" element={<ProfessorAuth />} />
+      <Route path="/professor-dashboard" element={<ProfessorDashboard />} />
+      <Route path="/professor-courses" element={<ProfessorCourses />} />
 
-        {/* Student Routes */}
-        <Route path="/student-login" element={<StudentAuth />} />
-        <Route path="/student-signup" element={<StudentAuth />} />
-        <Route path="/student-dashboard" element={<StudentDashboard />} />
-      </Routes>
-    </>
+      {/* Student Routes */}
+      <Route path="/student-login" element={<StudentAuth />} />
+      <Route path="/student-signup" element={<StudentAuth />} />
+      <Route path="/student-dashboard" element={<StudentDashboard />} />
+    </Routes>
   );
 };
 
